04f9d3f07af7c29bf30744a0dac9932d52eb5a37
Vallabh Mahajan <vallabhmahajan570@gmail.com>
2026-04-20 17:50:40 +0530

    Added SkipList module

---
+++ src/Memtable/SkipList.cpp
@@ -0,0 +1,141 @@
+#include "SkipList.hpp"
+#include <iostream>
+#include <random>
+
+namespace LSM {
+
+SkipList::SkipList(int max_level, float probability) : max_level(max_level), probability(probability), current_level(0){
+    head = new SkipListNode("", "", max_level);
+}
+
+SkipList::~SkipList(){
+    // Traverse at 0th level to ensure we delete all nodes
+    SkipListNode* current = head;
+    while(current != nullptr){
+        SkipListNode* next = current->forward[0];
+        delete current;
+        current = next;
+    }
+}
+int SkipList::randomLevel(){
+    // Generate a perfect random
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+
+    int lvl = 0;
+    while(dis(gen) < probability && lvl < max_level - 1){
+        lvl++;
+    }
+    return lvl;
+}
+
+std::optional<std::string> SkipList::get(const std::string &key) const{
+    SkipListNode* current = head;
+
+    // Start from highest active level and move right, then drop down
+    for(int i = current_level; i >= 0; i--){
+        while(current->forward[i] != nullptr && current->forward[i]->key < key){
+            current = current->forward[i];
+        }
+    }
+
+    // Drop to level 0 and advance to target node
+    current = current->forward[0];
+
+    if(current != nullptr && current->key == key){
+        return current->value;
+    }
+
+    return std::nullopt;
+}
+
+void SkipList::put(const std::string &key, const std::string &value){
+    // Keep track of nodes whose forward pointers need to be updated
+    std::vector<SkipListNode*> update(max_level, nullptr);
+    SkipListNode* current = head;
+
+    for(int i = current_level; i >= 0; i--){
+        while(current->forward[i] != nullptr && current->forward[i]->key < key){
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    current = current->forward[0];
+
+    // Case-1: key already exists, overwrite
+    if(current != nullptr && current->key == key){
+        current->value = value;
+        return;
+    }
+
+    // Case-2: key does not exits. insert a new node
+    int new_level = randomLevel();
+
+    // If new node level is higher than current maximum level of list, initialize update array for new levels to point to the head node
+    if(new_level > current_level){
+        for(int i = current_level + 1; i <= new_level; i++){
+            update[i] = head;
+        }
+        current_level = new_level;
+    }
+
+    // create new node
+    SkipListNode* new_node = new SkipListNode(key, value, new_level + 1);
+
+    for(int i = 0; i <= new_level; i++){
+        new_node->forward[i] = update[i]->forward[i];
+        update[i]->forward[i] = new_node;
+    }
+}
+
+void SkipList::remove(const std::string& key){
+    std::vector<SkipListNode*> update(max_level, nullptr);
+    SkipListNode* current = head;
+
+    for(int i = current_level; i >= 0; i--){
+        while(current->forward[i] != nullptr && current->forward[i]->key < key){
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    current = current->forward[0];
+
+    // If the node exists, update pointers to bypass it, then delete it.
+    if(current != nullptr && current->key == key){
+        for(int i = 0; i <= current_level; i++){
+            // If at level i, the previous node's forward pointer doesn't point to current, it means we've updated all pointers for levels the node existed in. Break early
+            if(update[i]->forward[i] != current){
+                break;
+            }
+            update[i]->forward[i] = current->forward[i];
+        }
+
+        delete current;
+
+        // Shrink current_level if the deleted node was the only one at the highest levels
+        while(current_level > 0 && head->forward[current_level] == nullptr){
+            current_level--;
+        }
+    }
+}
+
+void SkipList::print() const {
+    std::cout << "\n--- Skip List Contents --\n";
+
+    for(int i = current_level; i >= 0; i--){
+        SkipListNode* current = head->forward[i];
+        std::cout << "Level " << i << ": ";
+
+        while(current != nullptr){
+            std::cout << "[" << current->key << ":" << current->value << "] ";
+            current = current->forward[i];
+        }
+        std::cout << "\n";
+    }
+    std::cout << "------------------------\n";
+}
+
+} // namespace LSM
---
+++ src/Memtable/SkipList.hpp
@@ -0,0 +1,48 @@
+#ifndef SKIPLIST_HPP
+#define SKIPLIST_HPP
+
+#include <string>
+#include <vector>
+#include <optional>
+
+namespace LSM{
+
+struct SkipListNode{
+    std::string key;
+    std::string value;
+
+    // an Array of forward pointers.
+    std::vector<SkipListNode*> forward;
+
+    SkipListNode(const std::string &k, const std::string &v, int level) : key(k), value(v), forward(level, nullptr){}
+};
+
+class SkipList{
+private:
+    int max_level;
+    float probability;
+    int current_level;
+    SkipListNode* head;
+
+    // generate random level for a new node
+    int randomLevel();
+
+public:
+    SkipList(int max_level = 12, float probability = 0.25);
+    ~SkipList();
+
+    void put(const std::string &key, const std::string &value);
+
+    std::optional<std::string> get(const std::string &key) const;
+
+    // will make a tombstone but implementing this for now
+    void remove(const std::string &key);
+
+    // debugging
+    void print() const;
+
+};
+
+} // namespace LSM
+
+#endif
